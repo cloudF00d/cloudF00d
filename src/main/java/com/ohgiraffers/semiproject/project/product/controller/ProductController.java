ca5c04ca4827021406dd1a066242817a58cb42b8
DONGROCK <ehdfkr8022@naver.com>
2023-12-04 21:22:08 +0900

    타임리프로 html 화면 띄우기 진행중 중간 저장

---
+++ src/main/java/com/ohgiraffers/semiproject/project/product/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.semiproject.project.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("product")
+public class ProductController {
+
+    @GetMapping("productStory")
+    public String productStory(){
+
+        return "/content/project/product/productStory.html";
+    }
+
+    @GetMapping("productPage")
+    public String productPage(){
+
+        return "/content/project/product/productPage.html";
+    }
+
+}
