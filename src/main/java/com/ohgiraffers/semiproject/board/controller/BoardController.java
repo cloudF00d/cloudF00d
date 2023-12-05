92f478b2395df617b0ad18a492026f90b75fa985
DagyeongKang <gangda920@gmail.com>
2023-12-05 17:10:56 +0900

    manager connection

--- src/main/java/com/ohgiraffers/semiproject/board/controller/BoardController.java
+++ src/main/java/com/ohgiraffers/semiproject/board/controller/BoardController.java
@@ -5,17 +5,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/*")
+@RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("noticeboard")
-    public String defaultLocation(){
-        return "content/board/noticeboard/noticeMain";
+    @GetMapping("/complaintMain")
+    public String complaintBoard() {
+        return "content/board/noticeboard/complaintMain";
     }
 
-    @GetMapping("")
-    public String complaintBoard() {
+    @GetMapping("/complaintBoardAnswered")
+    public String complaintBoardAnswered() {
         return "content/board/noticeboard/complaintAnswered";
     }
+    @GetMapping("/complaintBoardBefore")
+    public String complaintBoardBefore() {
+        return "content/board/noticeboard/complaintBefore";
+    }
+
+    @GetMapping("/inquiryAnswered")
+    public String inquiryAnswered() {
+        return "content/board/inquiryboard/inquiryAnswered";
+    }
+
+    @GetMapping("/inquiryAnswering")
+    public String inquiryAnswering() {
+        return "content/board/inquiryboard/inquiryAnswering";
+    }
+
+    @GetMapping("/inquiryMain")
+    public String inquiryMain() {
+        return "content/board/inquiryboard/inquiryMain";
+    }
+
+    @GetMapping("/noticeMain")
+    public String noticeMain(){
+        return "content/board/noticeboard/noticeMain";
+    }
+    @GetMapping("/noticeWrited")
+    public String noticeWrited(){
+        return "content/board/noticeboard/noticeWrited";
+    }
+    @GetMapping("/noticeWriting")
+    public String noticeWriting(){
+        return "content/board/noticeboard/noticeWriting";
+    }
+
+
+
 }
 
