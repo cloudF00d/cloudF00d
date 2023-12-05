4a5296b6fcc37640108039c77a4d3017e7626ddc
22JunHee <lees8485@gmail.com>
2023-12-05 10:33:12 +0900

    멤버 파트 화면띄우기 진행중12

--- src/main/java/com/ohgiraffers/semiproject/member/controller/MemberController.java
+++ src/main/java/com/ohgiraffers/semiproject/member/controller/MemberController.java
@@ -26,4 +26,56 @@ public class MemberController {
         return "/content/member/login/join.html";
 
     }
+
+    @GetMapping("findId")
+    public String findId(){
+
+
+        return "/content/member/login/findId.html";
+
+    }
+
+
+    @GetMapping("findPwd")
+    public String findPwd(){
+
+
+        return "/content/member/login/findPwd.html";
+
+    }
+
+
+    @GetMapping("changeInfo")
+    public String changeInfo(){
+
+
+        return "/content/member/login/changeInfo.html";
+
+    }
+
+    @GetMapping("changeseller")
+    public String changeseller(){
+
+
+        return "/content/member/login/changeseller.html";
+
+    }
+
+    @GetMapping("sellerWithdrawal")
+    public String sellerWithdrawal(){
+
+
+        return "/content/member/login/sellerWithdrawal.html";
+
+    }
+
+    @GetMapping("userWithdrawal")
+    public String userWithdrawal(){
+
+
+        return "/content/member/login/userWithdrawal.html";
+
+    }
+
+
 }
