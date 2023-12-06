5acd97179215b6ea5c6af688f2410e19dc89ed59
DagyeongKang <gangda920@gmail.com>
2023-12-06 21:26:09 +0900

    searchUser try

--- src/main/java/com/ohgiraffers/semiproject/manager/search/controller/ManagerSearchController.java
+++ src/main/java/com/ohgiraffers/semiproject/manager/search/controller/ManagerSearchController.java
@@ -1,12 +1,22 @@
 package com.ohgiraffers.semiproject.manager.search.controller;
 
+import com.ohgiraffers.semiproject.manager.search.model.dto.UserDTO;
+import com.ohgiraffers.semiproject.manager.search.model.service.SearchUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/manager/search")
 public class ManagerSearchController {
+    private final SearchUserService searchUserService;
+
+    public ManagerSearchController(SearchUserService searchUserService) {
+        this.searchUserService = searchUserService;
+    }
 
     @GetMapping("/complaintDetail")
     public String complaintDetail(){
@@ -27,8 +37,13 @@ public class ManagerSearchController {
         return "/content/manager/search/userDetail";
     }
     @GetMapping("/userMain")
-    public String userMain(){
-        return "/content/manager/search/userMain";
+    public ModelAndView userMain(){
+        List<UserDTO> userDTOS = searchUserService.findAllUser();
+
+
+        return new ModelAndView("searchUser").addObject
+                ("viewpage","/userMain.html").addObject("userDTOS",userDTOS);
+//        "/content/manager/search/userMain";
     }
 }
 
---
+++ src/main/java/com/ohgiraffers/semiproject/manager/search/model/dao/SearchUserMapper.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.semiproject.manager.search.model.dao;
+
+import com.ohgiraffers.semiproject.manager.search.model.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SearchUserMapper {
+    /*
+    * 사용자 조회 기능
+    * 1. 게시판 형태의 폼에 모든 글을 출력한다.
+    * 2. 그 중 글을 클릭하면 글의 상세페이지를 출력하는 창으로 넘어간다.
+    * 3. 삭제할 수 있다?
+    * 4. 수정할 수 있다?
+    * 5. 정지할 수 있다?
+    * */
+
+    List<UserDTO> findAllUser();
+}
