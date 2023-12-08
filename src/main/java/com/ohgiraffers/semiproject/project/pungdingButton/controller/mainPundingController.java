6c12716dfcdde53ec1d660abcfa9d6518411159d
kimminha <kimmjnha@gmail.com>
2023-12-08 15:19:35 +0900

    23/12/08/15:19/image-05_page

---
+++ src/main/java/com/ohgiraffers/semiproject/project/pungdingButton/controller/mainPundingController.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.semiproject.project.pungdingButton.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pundingButton")
+public class mainPundingController{
+    @GetMapping("mainPunding")
+    public String mainPunding() {
+        return "content/project/pundingButton/mainPunding";
+    }
+}
