ffe9f39e0dbac82591416b0df8d712042392b9ae
22JunHee <lees8485@gmail.com>
2023-12-13 18:15:15 +0900

    회원가입 90퍼이상 완료??

---
+++ src/main/java/com/ohgiraffers/semiproject/member/emailsender/EmailSender.java
@@ -0,0 +1,60 @@
+package com.ohgiraffers.semiproject.member.emailsender;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class EmailSender {
+
+    public void sendEmail(String to, String from, String host, String subject, String text) {
+        // SMTP 서버 설정
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+
+        // 인증 정보 설정
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("lees8485", "penqoytsdahudarz");
+            }
+        });
+
+        try {
+            // 이메일 메시지 생성
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(text);
+            System.out.println("EmailSender 클래스 : "+to);
+            System.out.println("EmailSender 클래스 : "+text);
+            // 메시지 전송
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public boolean emailSend(String to, String pwdKey) {
+        // 이메일 발신자, 수신자, SMTP 호스트 및 메시지 내용 설정 to는 수신자
+        String from = "lees8485@gmail.com"; // 발신자 이메일 주소
+        String host = "smtp.gmail.com"; // SMTP 서버 주소
+        String subject = "CloudFooding 이메일 인증 번호"; // 이메일 제목
+        try{
+            sendEmail(to, from, host, subject, pwdKey);
+            return true;
+        } catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
+
+}
--- src/main/java/com/ohgiraffers/semiproject/member/model/service/MemberService.java
+++ src/main/java/com/ohgiraffers/semiproject/member/model/service/MemberService.java
@@ -5,18 +5,21 @@ import com.ohgiraffers.semiproject.common.exception.member.MemberModifyException
 import com.ohgiraffers.semiproject.common.exception.member.MemberRemoveException;
 import com.ohgiraffers.semiproject.member.model.dto.MemberAndAuthorityDTO;
 import com.ohgiraffers.semiproject.member.model.dto.MemberDTO;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface MemberService {
 
 
 
 
-    public void joinMember(MemberDTO member) throws MemberJoinException;
+    void joinMember(MemberDTO memberDTO) throws MemberJoinException;
 
-    public boolean selectMemberById(String userId);
+    boolean selectMemberById(String userId);
 
-    public void modifyMember(MemberAndAuthorityDTO member) throws MemberModifyException;
+    void modifyMember(MemberAndAuthorityDTO member) throws MemberModifyException;
 
-    public void removeMember(MemberAndAuthorityDTO member) throws MemberRemoveException;
+    void removeMember(MemberAndAuthorityDTO member) throws MemberRemoveException;
+
+    int sendEmail(String email);
+
+    boolean checkMail(String checkMail);
 }
