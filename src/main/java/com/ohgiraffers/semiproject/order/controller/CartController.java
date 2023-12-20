9de7c8efa8ebefda901df3f410fc0cfd90f651db
DONGROCK <ehdfkr8022@naver.com>
2023-12-20 19:47:21 +0900

    cart로 리워드정보  이동

--- src/main/java/com/ohgiraffers/semiproject/order/controller/CartController.java
+++ src/main/java/com/ohgiraffers/semiproject/order/controller/CartController.java
@@ -4,19 +4,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ohgiraffers.semiproject.common.exception.cart.CartRegistException;
 import com.ohgiraffers.semiproject.member.model.dto.MemberAndAuthorityDTO;
+import com.ohgiraffers.semiproject.member.model.dto.MemberDTO;
 import com.ohgiraffers.semiproject.order.model.dto.CartDTO;
 import com.ohgiraffers.semiproject.order.model.dto.SelectOptionDTO;
 import com.ohgiraffers.semiproject.order.model.dto.CartInsertDTO;
 import com.ohgiraffers.semiproject.order.model.service.CartService;
+import com.ohgiraffers.semiproject.project.model.dto.ProjectDTO;
 import com.ohgiraffers.semiproject.project.product.model.dto.ProjectOptionDTO;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.javassist.bytecode.annotation.MemberValue;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -51,42 +56,125 @@ public class CartController {
         model.addAttribute("cart", cartPage);
 
 
-
-
-
         return "/content/order/cart";
     }
 
 
+//    @PostMapping("cart")
+//    public String addToCart(@RequestParam int projectCode,
+//                            @RequestParam int optionCode,
+//                            HttpSession session, Model m,
+//                            @AuthenticationPrincipal MemberAndAuthorityDTO memberAndAuthorityDTO)
+//                             throws CartRegistException {
+////        m.addAttribute("projectCode",projectCode);
+////        m.addAttribute("optionType",optionType);
+////        m.addAttribute("optionCode",optionCode);
+////        System.out.println(optionCode+"========================================optionCode");
+//
+////        ProjectOptionDTO projectOption = selectedOption;
+////
+////        // CartDTO 객체 생성 및 설정
+//
+//        int userCode = memberAndAuthorityDTO.getMemberDTO().getUserCode();
+//
+//        CartDTO cart = new CartDTO();
+//        cart.getMemberCode().setUserCode(userCode);
+//        cart.getProjectOptionCode().setOptionCode(optionCode);
+//        cart.getProjectCode().setCode(projectCode);
+//        cartService.addToCart(cart);
+//
+//
+//
+//        return "redirect:/order/cart";
+//    }
+
+//    @PostMapping("cart")
+//    public String addToCart(@RequestParam int projectCode,
+//                            @RequestParam int optionCode,
+//                            HttpSession session, Model m,
+//                            @AuthenticationPrincipal MemberAndAuthorityDTO memberAndAuthorityDTO)
+//            throws CartRegistException {
+//
+//        int userCode = memberAndAuthorityDTO.getMemberDTO().getUserCode();
+//
+//        MemberDTO memberDTO = new MemberDTO();
+//        memberDTO.setUserCode(userCode); // MemberDTO 객체에 userCode 설정
+//
+//        CartDTO cart = new CartDTO();
+//        cart.setMemberCode(memberDTO); // 초기화된 MemberDTO 객체를 CartDTO에 설정
+//        cart.getProjectOptionCode().setOptionCode(optionCode);
+//        cart.getProjectCode().setCode(projectCode);
+//        cartService.addToCart(cart);
+//
+//        return "redirect:/order/cart";
+//
+//    }
+
+
+//    @PostMapping("cart")
+//    @ResponseBody
+//    public String addToCart(@RequestParam int projectCode,
+//                            @RequestParam int optionCode,
+//                            HttpSession session, Model m,
+//                            @AuthenticationPrincipal MemberAndAuthorityDTO memberAndAuthorityDTO)
+//            throws CartRegistException {
+//
+//        int userCode = memberAndAuthorityDTO.getMemberDTO().getUserCode();
+//
+//        MemberDTO memberDTO = new MemberDTO();
+//        memberDTO.setUserCode(userCode); // MemberDTO 객체에 userCode 설정
+//
+//        ProjectOptionDTO projectOptionDTO = new ProjectOptionDTO();
+//        projectOptionDTO.setOptionCode(optionCode); // ProjectOptionDTO 객체에 optionCode 설정
+//
+//        ProjectDTO projectDTO = new ProjectDTO();
+//        projectDTO.setCode(projectCode);
+//
+//        CartDTO cart = new CartDTO();
+//        cart.setMemberCode(memberDTO); // 초기화된 MemberDTO 객체를 CartDTO에 설정
+//        cart.setProjectOptionCode(projectOptionDTO); // 초기화된 ProjectOptionDTO 객체를 CartDTO에 설정
+//        cart.setProjectCode(projectDTO); // - 이 부분도 필요에 따라 유사하게 수정 필요
+//        System.out.println("cart = " + cart);
+//        cartService.addToCart(cart);
+//
+//        return "redirect:/order/cart";
+//    }
 
     @PostMapping("cart")
-    public String addToCart(@ModelAttribute ProjectOptionDTO selectedOption, HttpSession session, Model model) throws CartRegistException {
-        // 이제 selectedOption 객체를 사용하여 필요한 작업을 수행합니다.
-        // 예:
-//        int projectCode = selectedOption.getProjectCode();
-//        int optionCode = selectedOption.getOptionCode();
+    public String addToCart(@RequestParam int projectCode,
+                            @RequestParam int optionCode,
+                            HttpSession session, Model m,
+                            @AuthenticationPrincipal MemberAndAuthorityDTO memberAndAuthorityDTO)
+            throws CartRegistException {
 
-        // 로직 수행...
-        System.out.println("post cart 시작 합니다!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        log.info("Received selectedOption========================");
-        log.info("Received selectedOption: {}", selectedOption);
-        log.info("Received selectedOption========================");
 
-        cartService.addToCart(selectedOption, session);
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//
-//            // JSON 문자열을 Java 객체로 변환
-//            selectedOption = objectMapper.readValue(selectedOptionJson,ProjectOptionDTO.class);
-//
-//            // 변환된 객체를 사용하여 비즈니스 로직 수행
-//              cartService.addToCart(selectedOption, session);
-//
-//        } catch (JsonProcessingException e) {
-//            log.error("Error parsing selected option JSON", e);
-//            // 에러 처리 로직
-//        }
+
+
+        int userCode = memberAndAuthorityDTO.getMemberDTO().getUserCode();
+
+        Map<String, Object> addToCart = new HashMap<>();
+        addToCart.put("userCode", userCode);
+        addToCart.put("projectCode", projectCode);
+        addToCart.put("optionCode", optionCode);
+        System.out.println(projectCode + "===============================projectCode");
+        cartService.insertCart(addToCart);
+
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setUserCode(userCode); // MemberDTO 객체에 userCode 설정
+
+        ProjectOptionDTO projectOptionDTO = new ProjectOptionDTO();
+        projectOptionDTO.setOptionCode(optionCode); // ProjectOptionDTO 객체에 optionCode 설정
+
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setCode(projectCode);
+
+//        CartDTO cart = new CartDTO();
+//        cart.setMemberCode(memberDTO); // 초기화된 MemberDTO 객체를 CartDTO에 설정
+//        cart.setProjectOptionCode(projectOptionDTO); // 초기화된 ProjectOptionDTO 객체를 CartDTO에 설정
+//        cart.setProjectCode(projectDTO); // - 이 부분도 필요에 따라 유사하게 수정 필요
+//        System.out.println("cart = " + cart);
+//        cartService.addToCart(cart);
+
         return "redirect:/order/cart";
     }
-
 }
--- src/main/java/com/ohgiraffers/semiproject/order/model/service/CartService.java
+++ src/main/java/com/ohgiraffers/semiproject/order/model/service/CartService.java
@@ -3,14 +3,12 @@ package com.ohgiraffers.semiproject.order.model.service;
 import com.ohgiraffers.semiproject.common.exception.cart.CartRegistException;
 import com.ohgiraffers.semiproject.order.model.dao.CartMapper;
 import com.ohgiraffers.semiproject.order.model.dto.CartDTO;
-import com.ohgiraffers.semiproject.order.model.dto.SelectOptionDTO;
 import com.ohgiraffers.semiproject.order.model.dto.CartInsertDTO;
-import com.ohgiraffers.semiproject.project.product.model.dto.ProjectOptionDTO;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 
 @Service
@@ -20,7 +18,7 @@ public class CartService {
     private final CartMapper Mapper;
 
     public CartService(CartMapper mapper) {
-        Mapper = mapper;
+        this.Mapper = mapper;
     }
 
     public List<CartInsertDTO> cart() {
@@ -32,21 +30,36 @@ public class CartService {
     }
 
 
-    public void addToCart(ProjectOptionDTO selectedOption, HttpSession session) throws CartRegistException {
+    public void addToCart(CartDTO cart) throws CartRegistException {
 //        세션에서 cart라는 이름으로 저장된 cartDTO객체 가져온다.
-        CartDTO cart = (CartDTO) session.getAttribute("cart");
+//        CartDTO cart = (CartDTO) session.getAttribute("cart");
+
 
 //        샂용자의 세션에서 장바구니 가져오고 없으면 장바구니 생성
-        if (cart == null) {
-            cart = new CartDTO();
-        }
+//        if (cart == null) {
+//            cart = new CartDTO();
+//        }
+//
+//        log.info("================================ selectOption " + selectedOption);
+//        log.info("===================================== session " + session);
 
-        int result = Mapper.addToCart(selectedOption);
+        int result = Mapper.addToCart(cart);
+
+//        int result2 = Mapper.addToOption(session);
         if(!(result > 0)) {
             throw new CartRegistException("장바구니 등록에 실패하셨습니다.");
         }
 
+//
+//        session.setAttribute("cart", cart);
+    }
 
-        session.setAttribute("cart", cart);
+    public void insertCart(Map<String, Object> addToCart) {
+
+        int result = Mapper.insertCart(addToCart);
+
+        if (result > 0) {
+            System.out.println("성공!");
+        }
     }
 }
