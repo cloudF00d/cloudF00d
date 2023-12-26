7e694f4bda0b6dc6d3dc2ca0870dd2a1244a4ecd
22JunHee <lees8485@gmail.com>
2023-12-26 11:00:07 +0900

    다경님부분 가져오기

---
+++ src/main/java/com/ohgiraffers/semiproject/common/exception/thumbnail/ThumbnailRegistException.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.semiproject.common.exception.thumbnail;
+
+public class ThumbnailRegistException extends Exception {
+
+    public ThumbnailRegistException() {}
+
+    public ThumbnailRegistException(String msg) {
+        super(msg);
+    }
+}
